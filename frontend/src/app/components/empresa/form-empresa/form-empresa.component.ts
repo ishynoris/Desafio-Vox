e65dd191ccdd5ec270d2a40b595e7443bab428cb
Anailson Mota [W10] <anailsonmota@moobi.com.br>
2024-10-07 14:49:21 -0300

    Extrai implementação do formulário para um novo componente

---
+++ frontend/src/app/components/empresa/form-empresa/form-empresa.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, input, InputSignal, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Empresa, PayloadEmpresa } from '../../../interfaces/empresa.interface';
+
+@Component({
+  selector: 'app-form-empresa',
+  standalone: true,
+  imports: [
+	ReactiveFormsModule, 
+	MatFormFieldModule, 
+	MatInputModule, 
+	MatDatepickerModule, 
+	MatButtonModule 
+  ],
+  templateUrl: './form-empresa.component.html',
+  styleUrl: './form-empresa.component.css'
+})
+export class FormEmpresaComponent {
+
+	form!: FormGroup;
+	empresa!: Empresa | undefined;
+	empresaSignal: InputSignal<Empresa | undefined> = input<Empresa | undefined>();
+	@Output() eventConfirmacao = new EventEmitter<PayloadEmpresa>();
+
+	ngOnInit() {
+		this.empresa = this.empresaSignal();
+
+		this.form = new FormGroup({
+			nome: new FormControl<string>(this.empresa?.nome || "", {
+				nonNullable: true,
+				validators: Validators.required,
+			}),
+			cnpj: new FormControl<string>(this.empresa?.cnpj_mascara || "", {
+				nonNullable: true,
+				validators: Validators.required,
+			}),
+			dataFundacao: new FormControl<string>(this._getDataFundacao())
+		})
+	}
+
+	onConfirm() {
+		const payload = {
+			nome: this.form.controls['nome'].value,
+			cnpj: this.form.controls['cnpj'].value,
+			data_fundacao: this.form.controls['dataFundacao'].value
+		} as PayloadEmpresa;
+		this.eventConfirmacao.emit(payload)
+	}
+
+	private _getDataFundacao(): string {
+		if (this.empresa == null) {
+			return "";
+		}
+		return new Date(this.empresa.data_atualizacao_ptbr).toISOString();
+	}
+}
