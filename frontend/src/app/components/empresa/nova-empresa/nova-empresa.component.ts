e36411918c00fe522df6f443459d38f262b7aee8
Anailson Mota [W10] <anailsonmota@moobi.com.br>
2024-10-07 18:10:03 -0300

    Refatora componente para remover imports sem uso

--- frontend/src/app/components/empresa/nova-empresa/nova-empresa.component.ts
+++ frontend/src/app/components/empresa/nova-empresa/nova-empresa.component.ts
@@ -1,9 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { EmpresaService } from '../../../services/empresa.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
@@ -13,14 +8,7 @@ import { PayloadEmpresa } from '../../../interfaces/empresa.interface';
 @Component({
   selector: 'app-nova-empresa',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatButtonModule,
-    FormEmpresaComponent
-],
+  imports: [ FormEmpresaComponent ],
   templateUrl: './nova-empresa.component.html',
   styleUrl: './nova-empresa.component.css'
 })
@@ -28,18 +16,6 @@ export class NovaEmpresaComponent {
 	service = inject(EmpresaService);
 	snackBar = inject(MatSnackBar);
 	router = inject(Router);
-
-	form = new FormGroup({
-		nome: new FormControl<string>("", {
-			nonNullable: true,
-			validators: Validators.required,
-		}),
-		cnpj: new FormControl<string>("", {
-			nonNullable: true,
-			validators: Validators.required,
-		}),
-		dataFundacao: new FormControl<string>("")
-	})
 
 	onSalvar(payload: PayloadEmpresa) {
 		this.service.salvar(payload).subscribe(resp => {
